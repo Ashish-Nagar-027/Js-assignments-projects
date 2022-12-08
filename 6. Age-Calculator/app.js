b03267e00326754f392ecfb092b25a3b0eb78f84
Ashish Nagar <ashishnagar033@gmail.com>
2022-12-08 17:44:32 +0530

    age calculator fixed

--- 6. Age-Calculator/app.js
+++ 6. Age-Calculator/app.js
@@ -2,30 +2,43 @@ let input = document.getElementById("dob");
 let yearText = document.getElementById("years");
 let monthsText = document.getElementById("months");
 let DaysText = document.getElementById("days");
-// let error;
 
-let date = new Date();
-let yesterday =
-  date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + (date.getDate() - 1);
+let d1, m1, y1;
 
-input.setAttribute("max", yesterday);
-
-input.addEventListener("change", getAge);
+input.addEventListener("input", (e) => {
+  let DOB = new Date(e.target.value);
+  m1 = DOB.getMonth() + 1;
+  d1 = DOB.getDate();
+  y1 = DOB.getFullYear();
+  getAge();
+  console.log(d1, m1, y1);
+});
 
 function getAge(e) {
-  calcAge(e.target.value);
+  // calcAge(e.target.value);
+
+  calcAge();
 }
 
-function calcAge(DateOfBirth) {
-  //   console.log(DateOfBirth);
-  let dobTime = new Date(DateOfBirth);
-  let today = new Date();
-  let dateDiff = new Date(today - dobTime);
-  //   console.log(dateDiff);
-  let ageYear = dateDiff.getFullYear() - 1970;
-  let ageMonth = dateDiff.getMonth();
-  let ageDate = dateDiff.getDate();
+function calcAge() {
+  var date = new Date();
+  var d2 = date.getDate();
+  var m2 = 1 + date.getMonth();
+  var y2 = date.getFullYear();
+  var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
+  if (d1 > d2) {
+    d2 = d2 + month[m2 - 1];
+    m2 = m2 - 1;
+  }
+  if (m1 > m2) {
+    m2 = m2 + 12;
+    y2 = y2 - 1;
+  }
+
+  var ageDate = d2 - d1;
+  var ageMonth = m2 - m1;
+  var ageYear = y2 - y1;
   displayAge(ageYear, ageMonth, ageDate);
 }
 
